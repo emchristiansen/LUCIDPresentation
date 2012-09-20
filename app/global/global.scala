cda072dc81a223fc4e30c4004628d81b77d7c422
Eric Christiansen <ericmartinchristiansen@gmail.com>
2012-09-20 15:24:28 -0700

    removed references to my hard paths, instead user must set env variables

--- app/global/global.scala
+++ app/global/global.scala
@@ -7,14 +7,26 @@ import java.io.File
 import nebula._
 
 object PlayGlobal extends GlobalSettings {
-  // TODO: Make this system agnostic.
-  lazy val publicRoot = new File("/u/echristiansen/time/2012_08_19/LUCIDPresentation/public")
+  // TODO: Remove dependence on env variables.
+  val lucidPresentationRootEnv = System.getenv("LUCID_PRESENTATION_ROOT")
+  assert(lucidPresentationRootEnv != null, "You must define this environment variable")
+  val publicRoot = new File(lucidPresentationRootEnv + "/public")
+  assert(publicRoot.exists && publicRoot.isDirectory)
+
+  val runtimeConfigEnv = System.getenv("LUCID_RUNTIME_CONFIG")
+  assert(runtimeConfigEnv != null, "You must define this environment variable")
+  val runtimeConfigFile = new File(runtimeConfigEnv)
+  assert(runtimeConfigFile.exists && runtimeConfigFile.isFile)
+
+  val tableConfigDirectoryEnv = System.getenv("LUCID_TABLE_CONFIG_DIRECTORY")
+  assert(tableConfigDirectoryEnv != null, "You must define this environment variable")
+  val tableConfigDirectory = new File(tableConfigDirectoryEnv)
+  assert(tableConfigDirectory.exists && tableConfigDirectory.isDirectory)
+
+  ///////////////////////////////////////////////////////////  
 
   lazy val tableConfigFiles = {
-    val directory = new File(
-      Play.current.configuration.getString("experiment.tableConfigDirectory").get)
-    assert(directory.isDirectory)
-    directory.listFiles.filter(_.toString.endsWith(".scala")).toSeq
+    tableConfigDirectory.listFiles.filter(_.toString.endsWith(".scala")).toSeq
   }
 
   lazy val tableNames = tableConfigFiles.map(_.getName.replace(".scala", "").replace("table_", ""))
@@ -22,24 +34,24 @@ object PlayGlobal extends GlobalSettings {
   lazy val nameToFile: Map[String, File] = tableNames.zip(tableConfigFiles).toMap
 
   def nameToTableUnrendered(name: String): TableUnrendered = {
-    val experiments = 
+    val experiments =
       IO.interpretFile[Seq[Seq[CorrespondenceExperiment]]](nameToFile(name))
     TableUnrendered(experiments)
   }
 
-  lazy val nameToTableUnrenderedMemo: String => TableUnrendered = 
+  lazy val nameToTableUnrenderedMemo: String => TableUnrendered =
     Memoize(nameToTableUnrendered _)
 
   def nameToTable(name: String): Table = {
     val table = nameToTableUnrenderedMemo(name)
     def summarizers(experiment: CorrespondenceExperiment) = {
       Seq(
-	ExperimentSummaryDouble(
-	  "recognitionRate", 
-	  TableUnrendered.recognitionRate(experiment)),
-	ExperimentSummaryBufferedImage(
-	  "histogram",
-	  Histogram(experiment, "").render))
+        ExperimentSummaryDouble(
+          "recognitionRate",
+          TableUnrendered.recognitionRate(experiment)),
+        ExperimentSummaryBufferedImage(
+          "histogram",
+          Histogram(experiment, "").render))
     }
 
     table.render(summarizers)
@@ -51,8 +63,7 @@ object PlayGlobal extends GlobalSettings {
   }
 
   override def onStart(app: Application) {
-    RuntimeConfig.init(
-      new File(Play.current.configuration.getString("experiment.runtimeConfigFile").get))
+    RuntimeConfig.init(runtimeConfigFile)
 
     Logger.info("in play global")
     println("using println")
---
+++ setEnvironmentVariables.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+export LUCID_PRESENTATION_ROOT="/u/echristiansen/time/2012_08_19/LUCIDPresentation"
+export LUCID_RUNTIME_CONFIG="/u/echristiansen/week6/LUCID/config/runtime/runtime_eric.scala"
+export LUCID_TABLE_CONFIG_DIRECTORY="/u/echristiansen/week6/LUCID/config/tables"
